f4646e341c9440d196fecbab631ba2cf0235c5c2
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-03 13:24:24 +0700

    set webhook

--- setwebhook.php
+++ setwebhook.php
@@ -1,10 +1,11 @@
 <?php
 include("init.php");
-echo $webhook = $_SERVER['SERVER_NAME'] . "/" . basename(__DIR__) . "/" .f("get_config")("webhook");
-// echo "<pre>";
-// print_r(
-//     f("bot_kirim_perintah")("setWebhook",[
-//         'url'=>'',
-//         'drop_pending_updates'=true,
-//     ]);
-// )
+echo $webhook = "https://".$_SERVER['SERVER_NAME'] .f("get_config")("webhook");
+echo "<pre>";
+print_r(
+    f("bot_kirim_perintah")("setWebhook",[
+        'url'=>$webhook,
+        'drop_pending_updates'=>true,
+    ])
+);
+echo "</pre>";
