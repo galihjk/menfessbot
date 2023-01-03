cbb3441ec0212148ba967a66642a4c862ea1fa75
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-03 17:38:53 +0700

    tesdb

--- functions/db_connect.php
+++ functions/db_connect.php
@@ -1,8 +1,14 @@
 <?php
+mysqli_connect (
+    "localhost",
+    f("getconfig")("db_user"),
+    f("getconfig")("db_password"),
+    f("getconfig")("db_database"),
+);
 function db_connect(){
-    return mysql_connect (
-        "localhost",
-        f("getconfig")("db_user"),
-        f("getconfig")("db_password")
-    );
+    // return mysql_connect (
+    //     "localhost",
+    //     f("getconfig")("db_user"),
+    //     f("getconfig")("db_password")
+    // );
 }
