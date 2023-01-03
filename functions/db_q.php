9a2bce347544666d74f66ee8cd274594490a3f98
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-04 05:31:30 +0700

    fixing2

--- functions/db_q.php
+++ functions/db_q.php
@@ -3,10 +3,12 @@ function db_q($q){
     $mysqli = f("db_connect")();
     $data = [];
     if ($result = $mysqli -> query($q)) {
-        while ($row = $result -> fetch_row()) {
-            $data[] = $row;
+        if(method_exists($result,"fetch_row")){
+            while ($row = $result -> fetch_row()) {
+                $data[] = $row;
+            }
+            $result -> free_result();            
         }
-        $result -> free_result();
     }
     else{
         file_put_contents("ERROR DBQ ".date("YmdHis").".txt","$q\n\n".$mysqli->error);
