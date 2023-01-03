c810aab1abe4244b957c3738bb0d8644e6a118ad
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-03 13:21:52 +0700

    tes2

--- setwebhook.php
+++ setwebhook.php
@@ -1,6 +1,6 @@
 <?php
 include("init.php");
-echo $webhook = $_SERVER['SERVER_NAME'] . "/" . dirname(__FILE__) . "/" .f("get_config")("webhook");
+echo $webhook = $_SERVER['SERVER_NAME'] . "/" . basename(__DIR__) . "/" .f("get_config")("webhook");
 // echo "<pre>";
 // print_r(
 //     f("bot_kirim_perintah")("setWebhook",[
