65e8d2368e86a3079cfcdb36a73c55760200e3ef
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-04 13:16:04 +0700

    fixhome

--- functions/handle_callback_query.php
+++ functions/handle_callback_query.php
@@ -1,8 +1,11 @@
 <?php
 function handle_callback_query($botdata){
-    f("handle_botdata_functions")($botdata,[
+    if(!f("handle_botdata_functions")($botdata,[
         "handle_callback_query_profil",
-    ]);
+        "handle_callback_query_home",
+    ])){
+        file_put_contents("log/unhandledcallback_query_".date("Y-m-d-H-i").".txt", file_get_contents("php://input"));
+    };
     
 
 }
