22cedacf5f582977259d82ec562d47bdafddf8de
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-10 13:18:29 +0700

    bugfix

--- functions/handle_message.php
+++ functions/handle_message.php
@@ -34,7 +34,7 @@ function handle_message($botdata){
         }
     }
     elseif($botdata["chat"]["id"] == f("get_config")("groupdisc")){
-        file_put_contents("log/groupdisc".date("Y-m-d-H-i").".txt", print_r($botdata,true));
+        // file_put_contents("log/groupdisc".date("Y-m-d-H-i").".txt", print_r($botdata,true));
         $text = $botdata["text"] ?? "";
         if($text){
             $reply_to_message = $botdata['reply_to_message'];
@@ -47,17 +47,17 @@ function handle_message($botdata){
             
             $reply_to_message_id = $reply_to_message['message_id'] ?? [];
             $oleh = "";
-            file_put_contents("log/groupdiscuserentities".date("Y-m-d-H-i").".txt", print_r([$reply_to_message, $reply_to_message['entities'], $entities],true));
+            // file_put_contents("log/groupdiscuserentities".date("Y-m-d-H-i").".txt", print_r([$reply_to_message, $reply_to_message['entities'], $entities],true));
             foreach($entities as $entity){
                 if($entity['type'] == "text_link"){
                     $botuname = f("get_config")("botuname","");
-                    file_put_contents("log/groupdiscuser".date("Y-m-d-H-i").".txt", print_r([$entity['url'],str_replace("https://t.me/$botuname?start=","",$entity['url']),f("str_decrypt")(str_replace("https://t.me/$botuname?start=","",$entity['url']),true)],true));
+                    // file_put_contents("log/groupdiscuser".date("Y-m-d-H-i").".txt", print_r([$entity['url'],str_replace("https://t.me/$botuname?start=","",$entity['url']),f("str_decrypt")(str_replace("https://t.me/$botuname?start=","",$entity['url']),true)],true));
                     $oleh = f("str_decrypt")(str_replace("https://t.me/$botuname?start=","",$entity['url']),true);
                     break;
                 }
             }
             $komentator = $botdata['from']['first_name'] . (empty($botdata['from']['username'])?'':" (@".$botdata['from']['username'].")");
-            $url = f("channel_url")("/$reply_to_message_id?comment=".$botdata['message_id']);
+            $url = f("channel_url")("/$reply_to_message_id?comment=".$botdata['forward_from_message_id']);
             if($oleh){
                 f("bot_kirim_perintah")("sendMessage",[
                     "chat_id"=>$oleh,
