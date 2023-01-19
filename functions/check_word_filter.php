f24867259e9e989ae92ece3c259660184092949c
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-19 17:32:39 +0700

    worfd filter

--- functions/check_word_filter.php
+++ functions/check_word_filter.php
@@ -2,9 +2,11 @@
 function check_word_filter($word, $from){
     $wordfilter = strtolower(f("get_config")("wordfilter",""));
     $wordfilterarr = explode(",",$wordfilter);
-    $word = " ".strtolower(preg_replace('/[^a-zA-Z]/', " ", $word))." ";
+    $word1 = " ".strtolower(preg_replace('/[^a-zA-Z]/', " ", $word))." ";
     foreach($wordfilterarr as $item){
-        if(f("str_contains")($word, " $item ")){
+        if(f("str_contains")($word1, " $item ")
+        or f("str_contains")(" $word ", " $item ")
+        ){
             f("bot_kirim_perintah")("sendMessage",[
                 'chat_id'=>$from,
                 'text'=>"Gagal, pesan tidak boleh mengandung kata '$item'.",
