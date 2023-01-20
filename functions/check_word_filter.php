e36277319833e37fb271aeacde4121a2a403f50e
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-20 09:44:57 +0700

    wordfilter

--- functions/check_word_filter.php
+++ functions/check_word_filter.php
@@ -2,6 +2,19 @@
 function check_word_filter($text, $from){
     $wordfilter = strtolower(f("get_config")("wordfilter",""));
     $wordfilterarr = explode(",",strtolower($wordfilter));
+    foreach($wordfilterarr as $item){
+        if($item != ""){
+            if(f("str_contains")($text,$item)){
+                f("bot_kirim_perintah")("sendMessage",[
+                    'chat_id'=>$from,
+                    'text'=>"Gagal, pesan tidak boleh mengandung kata '$item'.",
+                    "parse_mode"=>"HTML",
+                ]);
+                return false;
+            }
+        }
+    }
+    /*
     $text = strtolower($text);
     $text1 = preg_replace('/[^a-zA-Z]/', " ", $text);
     foreach($wordfilterarr as $item){
@@ -16,5 +29,6 @@ function check_word_filter($text, $from){
             return false;
         }
     }
+    */
     return true;
 }
