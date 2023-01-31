aa917f7c642e27856f3d906ed728e68c69c6fdac
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-02-01 06:19:24 +0700

    check link

--- functions/check_link.php
+++ functions/check_link.php
@@ -3,13 +3,28 @@ function check_link($text, $from){
     if(f("get_config")("allow_link",false)){
         $result = f("bot_kirim_perintah")("sendMessage",[
             'chat_id'=>$from,
-            'text'=>"pesan tidak boleh mengandung link: $text",
+            'text'=>$text,
         ]);
-        f("bot_kirim_perintah")("sendMessage",[
+        if(!empty($result['result']['entities'])){
+            foreach($result['result']['entities'] as $ent){
+                if(!empty($ent['type']) and $ent['type'] == 'url'){
+                    f("bot_kirim_perintah")("editMessageText",[
+                        'chat_id'=>$from,
+                        'message_id'=>$result['result']['message_id'],
+                        'text'=>"Pesan tidak boleh mengandung link "
+                            .substr($result['result']['text'],$ent['offset'],$ent['length']),
+                        "parse_mode"=>"HTML",
+                        "disable_web_page_preview"=>true,
+                    ]);
+                    return false;
+                }
+            }
+        }
+        f("bot_kirim_perintah")("deleteMessage",[
             'chat_id'=>$from,
-            'text'=>"ini: ".print_r($result, true),
+            'message_id'=>$result['result']['message_id'],
         ]);
-        return false;
+
     }
     return true;
 }
